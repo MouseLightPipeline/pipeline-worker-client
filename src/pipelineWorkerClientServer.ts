a01b40bb7991b1862f57a7933bfa77d5b39f10fd
Patrick Edson <pedson@leapscientific.com>
2017-02-05 17:21:26 -0500

    Add staging configuration

--- src/pipelineWorkerClientServer.ts
+++ src/pipelineWorkerClientServer.ts
@@ -3,17 +3,19 @@ import * as WebpackDevServer from "webpack-dev-server";
 
 const config = require("./../webpack.config.js");
 
+const serverConfig = require("../system.config").default();
+
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
-    proxy: {"/graphql": `http://localhost:3001`},
+    proxy: {"/graphql": `http://localhost:${serverConfig.apiPort}`},
     historyApiFallback: true,
     noInfo: false,
     quiet: false
-}).listen(4001, "0.0.0.0", function (err, result) {
+}).listen(serverConfig.port, "0.0.0.0", function (err, result) {
     if (err) {
         return console.log(err);
     }
 
-    console.log("Listening at http://localhost:4001/");
+    console.log(`Listening at http://localhost:${serverConfig.port}/`);
 });
