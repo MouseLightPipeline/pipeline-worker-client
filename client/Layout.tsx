3ce0166104acc90096c1c522a4ee882d8d8af0fb
Patrick Edson <pedson@users.noreply.github.com>
2018-07-18 07:31:53 -0400

    Better display when backend is down.

--- client/Layout.tsx
+++ client/Layout.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {graphql} from "react-apollo";
-import {PageHeader} from "react-bootstrap";
+import {Alert, PageHeader} from "react-bootstrap";
 
 import {WorkerQuery} from "./GraphQLComponents";
 import {ExecutedTasks} from "./ExecutedTasks";
@@ -8,14 +8,33 @@ import {Loading} from "./Loading";
 import {RunningTasksWithQuery} from "./RunningTasks";
 
 class _Layout extends React.Component<any, any> {
+    public constructor(props: any) {
+        super(props);
+
+        this.state = {
+            hasError: false
+        }
+    }
+
     public render() {
-        const {loading, error} = this.props.data;
+        const {loading, error, networkStatus} = this.props.data;
 
         if (loading) {
             return <Loading/>
         }
 
-        const worker = this.props.data && !this.props.data.loading ? this.props.data.worker : null;
+        if (error) {
+            return (
+                <div style={{padding: "20px"}}>
+                    <Alert bsStyle="danger">
+                        <strong>{networkStatus === 8 ? "There is a problem connecting to the worker API process." : ""}</strong><br/>
+                        {error.message}
+                    </Alert>
+                </div>
+            );
+        }
+
+        const worker = this.props.data.worker;
 
         let divStyle = {
             margin: "20px"
@@ -34,5 +53,5 @@ class _Layout extends React.Component<any, any> {
 }
 
 export const Layout = graphql(WorkerQuery, {
-    options: {pollInterval: 5 * 1000}
+    options: {pollInterval: 10 * 1000}
 })(_Layout);
